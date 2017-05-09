17f45ef65701783bb466b5c1aa2a162890818b57
Gabriel <gortegagingrich@gmail.com>
2017-05-09 09:38:44 -0700

    I'll work on drawing textured cubes with this class. If you have something for the next checkpoint that you want to work on, feel free to create a class for it and claim it for yourself.

---
+++ src/TexturedCube.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Gabriel on 2017/05/09.
+ */
+public class TexturedCube extends Cube {
+	public TexturedCube(float x, float y, float z, float edgeLength) {
+		super(x, y, z, edgeLength);
+	}
+}
