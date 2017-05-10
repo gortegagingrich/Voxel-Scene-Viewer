aa40f20557ff6cebdbd4351124e1d0c4ff25f072
Gabriel Ortega-Gingrich <gortegagingrich@gmail.com>
2017-05-09 20:38:59 -0700

    Now supports binding textures differently based on type.

--- src/Cube.java
+++ src/Cube.java
@@ -13,63 +13,9 @@ import java.util.ArrayList;
  * position and edge length with random colored faces.
  *
  ****************************************************************/
-public class Cube  {
-	ArrayList<float[][]> faces;
-	private float[][] vertices;
-
-	// constructor: Cube
-	// purpose: creates 8 vertices for a cube at the given position with the given edge length
-	// then creates 6 faces with references to those vertices and random colors
-	public Cube(float x, float y, float z, float edgeLength) {
-		this.faces = new ArrayList<>();
-
-		/*
-		rough representation of which vertex is which:
-		 7---6
-		3---2|
-		|4--|5
-		0---1
-		 */
-		vertices = new float[][] {
-				  {x,y,z},
-				  {x+edgeLength,y,z},
-				  {x+edgeLength,y+edgeLength,z},
-				  {x,y+edgeLength,z},
-				  {x,y,z+edgeLength},
-				  {x+edgeLength,y,z+edgeLength},
-				  {x+edgeLength,y+edgeLength,z+edgeLength},
-				  {x,y+edgeLength,z+edgeLength}
-		};
-
-		// modified these to work with test texture
-		addFace(2,3,0,1);
-		addFace(6,2,1,5);
-		addFace(7,6,5,4);
-		addFace(3,7,4,0);
-		addFace(3,2,6,7);
-		addFace(5,4,0,1);
-	}
-
-	// method: addFace
-	// purpose: adds a face to the set of faces with given vertices and a random color
-	// Each face is represeted by a float[5][3], where the first four arrays are vertices
-	// and the final is a randomly generated color.
-	private void addFace(int v0, int v1, int v2, int v3) {
-		faces.add(new float[][] {
-				  vertices[v0],vertices[v1],vertices[v2],vertices[v3],
-				  {(float)Math.random(),(float)Math.random(),(float)Math.random()}
-		});
-	}
+public interface Cube  {
 
 	// method: draw
 	// purpose: for each face, draws a each vertex in the defined color
-	public synchronized void draw() {
-		faces.forEach(side -> {
-			GL11.glColor3f(side[4][0], side[4][1], side[4][2]);
-
-			for (int i = 0; i < 4; i++) {
-				GL11.glVertex3f(side[i][0], side[i][1], side[i][2]);
-			}
-		});
-	}
+	public void draw();
 }
--- src/TexturedCube.java
+++ src/TexturedCube.java
@@ -1,10 +1,12 @@
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureImpl;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
@@ -12,42 +14,148 @@ import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
 /**
  * Created by Gabriel on 2017/05/09.
  */
-public class TexturedCube extends Cube {
-	private static final HashMap<Integer, Texture> textureLibrary = new HashMap<>();
+public class TexturedCube implements Cube {
+	private int type;
+	ArrayList<float[][]> faces;
+	private float[][] vertices;
+
+	public static final int CUBE = 0;
+	private static final HashMap<Integer, Object[]> textureLibrary = new HashMap<>();
 
 	public TexturedCube(float x, float y, float z, float edgeLength) {
-		super(x, y, z, edgeLength);
+		this(x,y,z,edgeLength,CUBE);
+	}
+
+	public TexturedCube(float x, float y, float z, float edgeLength, int type) {
+		this.faces = new ArrayList<>();
+		this.type = type;
+
+		/*
+		rough representation of which vertex is which:
+		 7---6
+		3---2|
+		|4--|5
+		0---1
+		 */
+		vertices = new float[][] {
+				  {x,y,z},
+				  {x+edgeLength,y,z},
+				  {x+edgeLength,y+edgeLength,z},
+				  {x,y+edgeLength,z},
+				  {x,y,z+edgeLength},
+				  {x+edgeLength,y,z+edgeLength},
+				  {x+edgeLength,y+edgeLength,z+edgeLength},
+				  {x,y+edgeLength,z+edgeLength}
+		};
+
+		// modified these to work with test texture
+		addFace(2,3,0,1);
+		addFace(6,2,1,5);
+		addFace(7,6,5,4);
+		addFace(3,7,4,0);
+		addFace(3,2,6,7);
+		addFace(1,0,4,5);
 	}
 
 	public void draw() {
-		textureLibrary.get(0).bind();
+		float[][] side;
+		Object[] value;
+		float x,y,size;
 
-		faces.forEach(side -> {
-			GL11.glTexCoord2f(0,0);
+		value = textureLibrary.get(type);
+		((Texture)value[0]).bind();
+		x = (Float)value[1];
+		y = (Float)value[2];
+		size = (Float)value[3];
+
+		// assumes that the associated texture is of the following format:
+		// SIDES  TOP
+		// BOT    []
+
+		// sides
+		for (int i = 0; i < 4; i++) {
+			side = faces.get(i);
+
+			GL11.glTexCoord2f(x, y);
 			GL11.glVertex3f(side[0][0], side[0][1], side[0][2]);
 
-			GL11.glTexCoord2f(1,0);
+			GL11.glTexCoord2f(x+size, y);
 			GL11.glVertex3f(side[1][0], side[1][1], side[1][2]);
 
-			GL11.glTexCoord2f(1,1);
+			GL11.glTexCoord2f(x+size,y+size);
 			GL11.glVertex3f(side[2][0], side[2][1], side[2][2]);
 
-			GL11.glTexCoord2f(0,1);
+			GL11.glTexCoord2f(x,y+size);
 			GL11.glVertex3f(side[3][0], side[3][1], side[3][2]);
+		}
+
+		// top
+		side = faces.get(4);
+		GL11.glTexCoord2f(x+size,y);
+		GL11.glVertex3f(side[0][0], side[0][1], side[0][2]);
+
+		GL11.glTexCoord2f(x+2*size,y);
+		GL11.glVertex3f(side[1][0], side[1][1], side[1][2]);
+
+		GL11.glTexCoord2f(x+2*size,y+size);
+		GL11.glVertex3f(side[2][0], side[2][1], side[2][2]);
+
+		GL11.glTexCoord2f(x+size,y+size);
+		GL11.glVertex3f(side[3][0], side[3][1], side[3][2]);
+
+		// bot
+		side = faces.get(5);
+
+		GL11.glTexCoord2f(x,y+size);
+		GL11.glVertex3f(side[0][0], side[0][1], side[0][2]);
+
+		GL11.glTexCoord2f(x+size,y+size);
+		GL11.glVertex3f(side[1][0], side[1][1], side[1][2]);
+
+		GL11.glTexCoord2f(x+size,y+size*2);
+		GL11.glVertex3f(side[2][0], side[2][1], side[2][2]);
+
+		GL11.glTexCoord2f(x,y+size*2);
+		GL11.glVertex3f(side[3][0], side[3][1], side[3][2]);
+
+		// unbind texture
+		TextureImpl.bindNone();
+	}
+
+	private void addFace(int v0, int v1, int v2, int v3) {
+		faces.add(new float[][] {
+				  vertices[v0],vertices[v1],vertices[v2],vertices[v3],
+				  {(float)Math.random(),(float)Math.random(),(float)Math.random()}
 		});
 	}
 
 	public static void initTextureLibrary() {
 		Texture texture;
+		Object[] value;
+		float sideLength;
 
 		textureLibrary.clear();
 
+		System.out.println("Building texture library...");
+
 		try {
 			texture = TextureLoader.getTexture("PNG",
-			                                   ResourceLoader.getResourceAsStream("textures/test.png"));
-			textureLibrary.put(0,texture);
+			                                   ResourceLoader.getResourceAsStream("textures/block.png"));
+			sideLength = 32f/texture.getImageWidth();
+
+			// define for each type as follows:
+			addTextureEntry(CUBE, texture, 0f, 0f, sideLength);
+			addTextureEntry(1, texture,2*sideLength, 2*sideLength, sideLength);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void addTextureEntry(int type, Texture texture, float xOffset, float yOffset, float sideLength) {
+		Object[] value;
+
+		// {texture, xOffset, yOffset, sideLength}
+		value = new Object[] {texture, xOffset, yOffset, sideLength};
+		textureLibrary.put(type, value);
 	}
 }
